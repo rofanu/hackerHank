d072cfddc223d4a67e292d98cb2455eaa2a267a6
Rodolfo Nunes <rodolfofavnunes@hotmail.com>
2022-07-20 20:12:31 -0300

    Solving two problems of thirstDay OneWeekPrepationChallenge

---
+++ com/rodolfo/hackerHank/OneWeekPreparation/MinMaxSum.java
@@ -0,0 +1,29 @@
+package com.rodolfo.hackerHank.OneWeekPreparation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MinMaxSum {
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        miniMaxSum(arr);
+    }
+
+    public static void miniMaxSum(List<Integer> arr) {
+        Long sum = 0L;
+        Long maxValue = Long.MIN_VALUE;
+        Long minValue = Long.MAX_VALUE;
+        for (Integer integer : arr) {
+            if (integer > maxValue)
+                maxValue = integer.longValue();
+            if (integer < minValue)
+                minValue = integer.longValue();
+            sum = sum + integer;
+        }
+        Long maxSum = sum - minValue;
+        Long minSum = sum - maxValue;
+
+        System.out.print(minSum + " " + maxSum);
+    }
+}
---
+++ com/rodolfo/hackerHank/OneWeekPreparation/PlusMinus.java
@@ -0,0 +1,40 @@
+package com.rodolfo.hackerHank.OneWeekPreparation;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class PlusMinus {
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>(Arrays.asList(-4, 3, -9, 0, 4, 1));
+        plusMinus(arr);
+    }
+
+    public static void plusMinus(List<Integer> arr) {
+        // Write your code here
+        Integer positives = 0;
+        Integer negatives = 0;
+        Integer zeros = 0;
+
+        Integer total = arr.size();
+        for (Integer number : arr) {
+            if (number > 0)
+                positives++;
+            else if (number < 0)
+                negatives++;
+            else
+                zeros++;
+        }
+
+        DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.ENGLISH);
+        DecimalFormat df = new DecimalFormat("0.0000", dfs);
+        System.out.println(df.format(positives.floatValue()/total.floatValue()));
+        System.out.println(df.format(negatives.floatValue()/total.floatValue()));
+        System.out.println(df.format(zeros.floatValue()/total.floatValue()));
+    }
+
+
+}
